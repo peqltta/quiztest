b367b456c088c08f6964a0c7f1cc15b4d3e7137c
peqltta <peqlkush@protonmail.com>
2021-07-01 20:23:02 +0100

    fdsaf

--- quizv0.01.py
+++ quizv0.01.py
@@ -3,16 +3,16 @@ with open('textlist.txt', 'r', encoding="utf-8") as f:
     y = x.replace('\n',': ')
     z = y.split(': ')
     f.close()
-    
+print("\x1b[1;31;40m"+"Welcome to the AWS Cloud Practitioner 2021 Complete Quiz."+"\x1b[0m") 
 for i in z:
     if "Question #" in i:
-        input("Press Enter to continue..")
-        print("\n"+i)
+        input("\x1b[2;30;44m"+"Press Enter to continue.."+"\x1b[0m")
+        print("\n"+"\x1b[6;30;42m"+i+"\x1b[0m")
     elif "Correct Answer" in i:
-        input("Input your Answer..")
-        print(i)
+        input("\x1b[1;32;40m"+"Input your Answer..   "+"\x1b[0m")
+        print("\x1b[1;34;40m"+i+"\x1b[0m")
     elif "Reference" in i:
-        input("Press Enter to continue..")
+        input("\x1b[2;30;44m"+"Press Enter to continue.."+"\x1b[0m")
         print(i)
     else:
         print(i)
