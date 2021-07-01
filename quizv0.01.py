33e56446d6109320fa6a0fce63dd0d3dcc9a3e1c
peqltta <peqlkush@protonmail.com>
2021-07-01 23:05:26 +0100

    timer

--- quizv0.01.py
+++ quizv0.01.py
@@ -3,10 +3,10 @@ def countdown(time_sec):
     while time_sec:
         mins, secs = divmod(time_sec, 60)
         timeformat = '{:02d}:{:02d}'.format(mins, secs)
-        print(timeformat, end='\r')
+        print("Do not press anything. " + timeformat, end='\r')
         time.sleep(1)
         time_sec -= 1
-        
+    print("")       
 with open('textlist.txt', 'r', encoding="utf-8") as f:
     x = f.read()
     y = x.replace('\n',': ')
