f72d1f86f313f6a38e69c70ccc657ed8f60e5184
peqltta <85615860+peqltta@users.noreply.github.com>
2021-07-01 20:03:02 +0100

    delete pdfdump.py

--- pdfdump.py
+++
@@ -1,8 +0,0 @@
-import fitz  # this is pymupdf
-
-with fitz.open("1.pdf") as doc:
-    text = ""
-    for page in doc:
-        text += page.getText()
-with open('1.txt', 'w+', encoding="utf-8") as f:
-    f.write(text)
