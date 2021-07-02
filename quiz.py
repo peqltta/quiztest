7787772d90c97e00a08f5f50fec2d34256e6c212
peqltta <peqlkush@protonmail.com>
2021-07-02 21:31:10 +0100

    randomness logic

--- quiz.py
+++ quiz.py
@@ -1,57 +1,66 @@
-import time
-def countdown(time_sec):
-    while time_sec:
-        mins, secs = divmod(time_sec, 60)
-        timeformat = '{:02d}:{:02d}'.format(mins, secs)
-        print("Do not press anything. " + timeformat, end='\r')
-        time.sleep(1)
-        time_sec -= 1
-    print("")       
+import random
+print("")
+print("Welcome to the Cloud Practitioner Quiz. 65 Questions. For Multiple choice answer in alphabetical order i.e AB, CE, BD")
+QADict = {}
 with open('textlist.txt', 'r', encoding="utf-8") as f:
     x = f.read()
     y = x.replace('\n',': ')
     z = y.split(': ')
     f.close()
-print("Welcome to the AWS Cloud Practitioner 2021 Complete Quiz.")
-progress = 1
-print("Input Progress (Question start #)")
-progress = input()
-timeinp = 0
-while not timeinp:
-    try:
-        print("How long per question? (Default 83s -- 90 Minutes / 65 Questions)")
-        timeinp = int(input())
-    except Exception as e:
-        print('Please enter an integer in seconds')    
-started = "no"
 for i in z:
-    if "no" in started:
-        if ("Question #"+progress) not in i:
-            continue
-        if ("Question #"+progress) in i:
-            started = "yes"
-    if "yes" in started:
-        if "Question #" in i:
-            input("Press Enter to continue..")
+    if "Question #" in i:
+        Ques = i
+        qqq = 1
+    elif "Correct Answer" in i:
+        qqq = 0
+        ccc = 1
+    elif "Reference" in i:
+        ccc = 0
+        rrr = 1
+    elif "Explanation" in i:
+        rrr = 0
+        eee = 1
+    elif "https://" in i:
+        QADict[Ques+"Ref"] = i
+    else:
+        if qqq==1:
+            if "A. " in i:
+                QADict[Ques+"A"] = i
+            elif "B. " in i:
+                QADict[Ques+"B"] = i
+            elif "C. " in i:
+                QADict[Ques+"C"] = i
+            elif "D. " in i:
+                QADict[Ques+"D"] = i
+            elif "E. " in i:
+                QADict[Ques+"E"] = i
+            else:
+                QADict[Ques] = i
+        elif ccc==1:
+            QADict[Ques+"Answer"] = i
+        elif rrr==1:
+            QADict[Ques+"Ref"] = i
+for i in range(1,65):
+    numran = random.randint(1,671)
+    print(QADict["Question #"+str(numran)])
+    print("")
+    print(QADict["Question #"+str(numran)+"A"])
+    print(QADict["Question #"+str(numran)+"B"])
+    print(QADict["Question #"+str(numran)+"C"])
+    print(QADict["Question #"+str(numran)+"D"])
+    try:
+        print(QADict["Question #"+str(numran)+"E"])
+    except:
+        hi = 1
+    print("")
+    correct = 0
+    while correct == 0:    
+        ans = input("Please Input your Answer ")
+        print("")
+        if ans == QADict["Question #"+str(numran)+"Answer"]:
+            print("You are right! ")
             print("")
-            print("\n"+i)
-        elif "Correct Answer" in i:
-            print("")
-            countdown(timeinp)
-            ans = input("Input your Answer..   ")
-            print("")
-            print("Your Answer: "+ans)
-            print("")
-            print(i)
-        elif "Reference" in i:
-            input("Press Enter to continue..")
-            print("")
-            print(i)
-        elif "Explanation" in i:
-            print(i)
-            print("")
-        elif "https://" in i:
-            print(i)
-            print("")
+            correct = 1
         else:
-            print(i)
+            print("Wrong guess again! ")
+            print("")
